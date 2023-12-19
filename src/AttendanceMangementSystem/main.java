d008e2205571b06937f4344ccd216c92507acd23
Baor1611 <thaibao1611@gmail.com>
2023-12-19 17:28:11 +0700

    Change to remote server

--- src/AttendanceMangementSystem/main.java
+++ src/AttendanceMangementSystem/main.java
@@ -9,11 +9,11 @@ package AttendanceMangementSystem;
  * @author nguye
  */
 public class main {
-    static String dbUsername = "sa";
-    static String dbPassword = "sa";
+    static String dbUsername = "baor1611_";
+    static String dbPassword = "baor1611";
 
-    static String jdbcUrl = "jdbc:sqlserver://localhost:1434;databaseName=AttendanceManagementSystem;user=" + dbUsername + ";password=" + dbPassword;
-
+    //static String jdbcUrl = "jdbc:sqlserver://localhost:1434;databaseName=AttendanceManagementSystem;user=" + dbUsername + ";password=" + dbPassword;
+    static String jdbcUrl = "jdbc:sqlserver://sql.bsite.net\\MSSQL2016;databaseName=baor1611_;user=baor1611_;password=baor1611;encrypt=true;trustServerCertificate=true;";
     public static void main(String[] args) {
         LoginFrame loginFrame = new LoginFrame();
         loginFrame.setVisible(true);
